aba536dfc8413095a5a76f95410940e57b926b67
Nico Mamone <nicolasmamone@gmail.com>
2023-12-18 23:40:52 -0300

    Configuramos los servicios

--- src/main/java/com/nico/implpatrondto/ImplPatronDtoApplication.java
+++ src/main/java/com/nico/implpatrondto/ImplPatronDtoApplication.java
@@ -1,21 +1,21 @@
 package com.nico.implpatrondto;
 
-import com.nico.implpatrondto.entities.Cliente;
-import com.nico.implpatrondto.entities.CuentaActual;
-import com.nico.implpatrondto.entities.CuentaDeAhorro;
-import com.nico.implpatrondto.entities.OperacionCuenta;
+import com.nico.implpatrondto.entities.*;
 import com.nico.implpatrondto.enums.EstadoCuenta;
 import com.nico.implpatrondto.enums.TipoDeOperacion;
+import com.nico.implpatrondto.exceptions.ClienteNotFoundException;
 import com.nico.implpatrondto.repositories.ClienteRepository;
 import com.nico.implpatrondto.repositories.CuentaBancariaRepository;
 import com.nico.implpatrondto.repositories.OperacionCuentaRepository;
 import com.nico.implpatrondto.services.BancoService;
+import com.nico.implpatrondto.services.CuentaBancariaService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 import java.util.stream.Stream;
 
@@ -26,7 +26,7 @@ public class ImplPatronDtoApplication {
         SpringApplication.run(ImplPatronDtoApplication.class, args);
     }
 
-    @Bean
+    //@Bean
     CommandLineRunner commandLineRunner(BancoService bancoService){
         return args -> bancoService.consultar();
     }
@@ -76,6 +76,37 @@ public class ImplPatronDtoApplication {
 
                 }
             });
+        };
+    }
+
+    @Bean
+    CommandLineRunner start2(CuentaBancariaService cuentaBancariaService){
+        return args -> {
+            Stream.of("Pepe", "Juan", "Pedro", "Marcos").forEach(nombre ->{
+                Cliente cliente = new Cliente();
+                cliente.setNombre(nombre);
+                cliente.setEmail(nombre+"@gmail.com");
+                cuentaBancariaService.saveCliente(cliente);
+            });
+            cuentaBancariaService.listClientes().forEach(cliente ->{
+                try{
+                    cuentaBancariaService.saveCuentaBancariaActual(Math.random() * 90000, 9000, cliente.getId());
+                    cuentaBancariaService.saveCuentaBancariaAhorro(120000, 5.5, cliente.getId());
+
+                    List<CuentaBancaria> cuentasBancarias = cuentaBancariaService.listCuentasBancarias();
+
+                    for (CuentaBancaria cuentaBancaria : cuentasBancarias){
+                        for (int i = 0; i < 10; i++) {
+                            cuentaBancariaService.credit(cuentaBancaria.getId(), 10000+Math.random()*120000, "Credito");
+                            cuentaBancariaService.debit(cuentaBancaria.getId(), 1000+Math.random()*9000, "Debito");
+
+                        }
+                    }
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            });
+
         };
     }
 }
